8aa1ce72463fb3b62f68d58ece3dc08793a627a2
Mohammad Quadri <quadrixm@gmail.com>
2021-09-12 20:28:40 +0530

    adding react bootstrap

--- frontend/src/components/ViewLogin.tsx
+++ frontend/src/components/ViewLogin.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {AUTH_USER_ID_KEY, ROOT_PATH} from "../constants";
 import {Redirect} from "react-router-dom";
+import {Button, Col, Container, Row} from "react-bootstrap";
 
 export const ViewLogin = () => {
   const signedIn = localStorage.getItem(AUTH_USER_ID_KEY) && localStorage.getItem(AUTH_USER_ID_KEY)?.length;
@@ -8,6 +9,12 @@ export const ViewLogin = () => {
     return <Redirect to={ROOT_PATH} />;
   }
   return (
-    <div>Login</div>
+    <Container>
+      <Row>
+        <Col>
+          <Button variant="primary">Login</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
